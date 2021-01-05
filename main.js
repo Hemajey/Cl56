cc6a5c9863ae0b844a8af3fc1b7e307b8487d33f
Hemajey <76666515+Hemajey@users.noreply.github.com>
2021-01-05 18:38:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function prac1() {
+    document.getElementById("head").style.color = "green";
+}
+
+function bclk() {
+    document.getElementById("but").style.backgroundColor = "pink";
+    document.getElementById("but").innerHTML="Well done!";
+}
